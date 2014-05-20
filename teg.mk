0155a3d04231f97b97a28bc15427e941afaf7c33
Solra Bizna <solra@bizna.name>
2014-05-19 23:13:19 -0600

    Make it work?

--- teg.mk
+++ teg.mk
@@ -4,4 +4,4 @@ ARFLAGS=-rscD
 obj/libteg.a: obj/teg/config.o obj/teg/io.o obj/teg/miscutil.o obj/teg/video.o obj/teg/xgl.o obj/teg/main.o
 	@echo Archiving "$@"...
 	@$(AR) $(ARFLAGS) "$@" $(filter %.o,$^)
-	@test -f obj/teg/config.debug.o && $(AR) $(ARFLAGS) $(patsubst %.a,%.debug.a,"$@") $(patsubst %.o,%.debug.o,$^)
+	@test -f obj/teg/config.debug.o && $(AR) $(ARFLAGS) $(patsubst %.a,%.debug.a,"$@") $(patsubst %.o,%.debug.o,$(filter %.o,$^))
