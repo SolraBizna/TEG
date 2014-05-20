dfffb139cc3d66bea13225378228e7361a58ac00
Solra Bizna <solra@bizna.name>
2014-05-19 22:51:39 -0600

    Support debugging build (2)

--- teg.mk
+++ teg.mk
@@ -7,4 +7,4 @@ obj/teg:
 obj/libteg.a: obj/teg obj/teg/config.o obj/teg/io.o obj/teg/miscutil.o obj/teg/video.o obj/teg/xgl.o obj/teg/main.o
 	@echo Archiving "$@"...
 	@$(AR) $(ARFLAGS) "$@" $(filter %.o,$^)
-	@test -f obj/teg/config.debug.o && $(AR) $(ARFLAGS) $(patsubst %.a,%.debug.a," $@") $(patsubst %.o,%.debug.o,$^)
+	@test -f obj/teg/config.debug.o && $(AR) $(ARFLAGS) $(patsubst %.a,%.debug.a,"$@") $(patsubst %.o,%.debug.o,$^)
