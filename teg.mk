e9093c1b7ff5921b9a93db8b60da28505d0139b9
Solra Bizna <solra@bizna.name>
2014-05-19 22:53:42 -0600

    We can't save everyone

--- teg.mk
+++ teg.mk
@@ -1,10 +1,7 @@
 AR=ar
 ARFLAGS=-rscD
 
-obj/teg:
-	@mkdir obj/teg
-
-obj/libteg.a: obj/teg obj/teg/config.o obj/teg/io.o obj/teg/miscutil.o obj/teg/video.o obj/teg/xgl.o obj/teg/main.o
+obj/libteg.a: obj/teg/config.o obj/teg/io.o obj/teg/miscutil.o obj/teg/video.o obj/teg/xgl.o obj/teg/main.o
 	@echo Archiving "$@"...
 	@$(AR) $(ARFLAGS) "$@" $(filter %.o,$^)
 	@test -f obj/teg/config.debug.o && $(AR) $(ARFLAGS) $(patsubst %.a,%.debug.a,"$@") $(patsubst %.o,%.debug.o,$^)
