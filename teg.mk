c1ecf77101660f1cf432ae60adffd6a92832165e
Solra Bizna <solra@bizna.name>
2014-05-19 23:20:48 -0600

    Fix debug build

--- teg.mk
+++ teg.mk
@@ -4,4 +4,4 @@ ARFLAGS=-rscD
 lib/libteg.a: obj/teg/config.o obj/teg/io.o obj/teg/miscutil.o obj/teg/video.o obj/teg/xgl.o obj/teg/main.o
 	@echo Archiving "$@"...
 	@$(AR) $(ARFLAGS) "$@" $^
-	@test -f obj/teg/config.debug.o && $(AR) $(ARFLAGS) $(patsubst %.a,%.debug.a,"$@") $(patsubst %.o,%.debug.o,$^)
+	@test -f obj/teg/config.debug.o && $(AR) $(ARFLAGS) $(patsubst %.a,%.debug.a,$@) $(patsubst %.o,%.debug.o,$^)
