4629949b35e2e2fb2ce978346e2f2e71a58ebcee
Solra Bizna <solra@bizna.name>
2014-05-19 23:15:24 -0600

    Better path

--- teg.mk
+++ teg.mk
@@ -1,7 +1,7 @@
 AR=ar
 ARFLAGS=-rscD
 
-obj/libteg.a: obj/teg/config.o obj/teg/io.o obj/teg/miscutil.o obj/teg/video.o obj/teg/xgl.o obj/teg/main.o
+lib/libteg.a: obj/teg/config.o obj/teg/io.o obj/teg/miscutil.o obj/teg/video.o obj/teg/xgl.o obj/teg/main.o
 	@echo Archiving "$@"...
-	@$(AR) $(ARFLAGS) "$@" $(filter %.o,$^)
-	@test -f obj/teg/config.debug.o && $(AR) $(ARFLAGS) $(patsubst %.a,%.debug.a,"$@") $(patsubst %.o,%.debug.o,$(filter %.o,$^))
+	@$(AR) $(ARFLAGS) "$@" $^
+	@test -f obj/teg/config.debug.o && $(AR) $(ARFLAGS) $(patsubst %.a,%.debug.a,"$@") $(patsubst %.o,%.debug.o,$^)
