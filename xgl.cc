a8a1b3086b61b192779726059b2bab3694cbb64e
Solra Bizna <solra@bizna.name>
2014-05-24 09:59:31 -0600

    Maybe support shaders now?

--- xgl.cc
+++ xgl.cc
@@ -169,8 +169,8 @@ void xgl::Initialize() {
   else {
   no_ARB_vertex_buffer_object:
     have_ARB_vertex_buffer_object = false;
-    fprintf(stderr, "xgl: Missing optional GL extension"
-            " ARB_vertex_buffer_object\n");
+  }
+  if(!have_ARB_vertex_buffer_object) {
     stub_proc_address(BindBuffer);
     stub_proc_address(DeleteBuffers);
     stub_proc_address(GenBuffers);
@@ -189,5 +189,218 @@ void xgl::Initialize() {
     if(blacklisted_extension_list.count("GL_ARB_texture_rectangle"))
       blacklisted_core_extension_warning("GL_ARB_texture_rectangle",2,1);
     have_ARB_texture_rectangle = true;
+  } else have_ARB_texture_rectangle = false;
+  /* ARB_shader_objects */
+  if(MIN_VERSION(2, 0)
+     || chunky_extension_list.count("GL_ARB_shader_objects")) {
+    if(blacklisted_extension_list.count("GL_ARB_shader_objects"))
+      blacklisted_core_extension_warning("GL_ARB_shader_objects",2,0);
+    have_ARB_shader_objects = true;
+    set_proc_address_ARB(no_ARB_shader_objects, DeleteObject);
+    set_proc_address_ARB(no_ARB_shader_objects, GetHandle);
+    set_proc_address_ARB(no_ARB_shader_objects, DetachObject);
+    set_proc_address_ARB(no_ARB_shader_objects, CreateShaderObject);
+    set_proc_address_ARB(no_ARB_shader_objects, ShaderSource);
+    set_proc_address_ARB(no_ARB_shader_objects, CompileShader);
+    set_proc_address_ARB(no_ARB_shader_objects, CreateProgramObject);
+    set_proc_address_ARB(no_ARB_shader_objects, AttachObject);
+    set_proc_address_ARB(no_ARB_shader_objects, LinkProgram);
+    set_proc_address_ARB(no_ARB_shader_objects, UseProgramObject);
+    set_proc_address_ARB(no_ARB_shader_objects, ValidateProgram);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform1f);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform2f);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform3f);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform4f);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform1i);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform2i);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform3i);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform4i);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform1fv);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform2fv);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform3fv);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform4fv);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform1iv);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform2iv);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform3iv);
+    set_proc_address_ARB(no_ARB_shader_objects, Uniform4iv);
+    set_proc_address_ARB(no_ARB_shader_objects, UniformMatrix2fv);
+    set_proc_address_ARB(no_ARB_shader_objects, UniformMatrix3fv);
+    set_proc_address_ARB(no_ARB_shader_objects, UniformMatrix4fv);
+    set_proc_address_ARB(no_ARB_shader_objects, GetObjectParameterfv);
+    set_proc_address_ARB(no_ARB_shader_objects, GetObjectParameteriv);
+    set_proc_address_ARB(no_ARB_shader_objects, GetInfoLog);
+    set_proc_address_ARB(no_ARB_shader_objects, GetAttachedObjects);
+    set_proc_address_ARB(no_ARB_shader_objects, GetUniformLocation);
+    set_proc_address_ARB(no_ARB_shader_objects, GetActiveUniform);
+    set_proc_address_ARB(no_ARB_shader_objects, GetUniformfv);
+    set_proc_address_ARB(no_ARB_shader_objects, GetUniformiv);
+    set_proc_address_ARB(no_ARB_shader_objects, GetShaderSource);
+    /* ARB_vertex_shader */
+    if(chunky_extension_list.count("GL_ARB_vertex_shader")) {
+      if(blacklisted_extension_list.count("GL_ARB_vertex_shader"))
+        blacklisted_core_extension_warning("GL_ARB_vertex_shader",2,0);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib1f);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib1s);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib1d);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib2f);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib2s);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib2d);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib3f);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib3s);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib3d);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4f);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4s);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4d);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4Nub);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib1fv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib1sv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib1dv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib2fv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib2sv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib2dv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib3fv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib3sv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib3dv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4fv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4sv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4dv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4iv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4bv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4ubv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4usv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4uiv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4Nbv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4Nsv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4Niv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4Nubv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4Nusv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttrib4Nuiv);
+      set_proc_address_ARB(no_ARB_vertex_shader, VertexAttribPointer);
+      set_proc_address_ARB(no_ARB_vertex_shader, EnableVertexAttribArray);
+      set_proc_address_ARB(no_ARB_vertex_shader, DisableVertexAttribArray);
+      set_proc_address_ARB(no_ARB_vertex_shader, BindAttribLocation);
+      set_proc_address_ARB(no_ARB_vertex_shader, GetActiveAttrib);
+      set_proc_address_ARB(no_ARB_vertex_shader, GetAttribLocation);
+      set_proc_address_ARB(no_ARB_vertex_shader, GetVertexAttribdv);
+      set_proc_address_ARB(no_ARB_vertex_shader, GetVertexAttribfv);
+      set_proc_address_ARB(no_ARB_vertex_shader, GetVertexAttribiv);
+      set_proc_address_ARB(no_ARB_vertex_shader, GetVertexAttribPointerv);
+    }
+    else {
+    no_ARB_vertex_shader:
+      have_ARB_vertex_shader = false;
+    }
+    /* ARB_fragment_shader */
+    if(chunky_extension_list.count("GL_ARB_fragment_shader")) {
+      if(blacklisted_extension_list.count("GL_ARB_fragment_shader"))
+        blacklisted_core_extension_warning("GL_ARB_fragment_shader",2,0);
+      have_ARB_fragment_shader = true;
+    } else have_ARB_fragment_shader = false;
+  }
+  else {
+  no_ARB_shader_objects:
+    have_ARB_shader_objects = false;
+    have_ARB_vertex_shader = false;
+    have_ARB_fragment_shader = false;
+  }
+  if(!have_ARB_shader_objects) {
+    stub_proc_address(DeleteObject);
+    stub_proc_address(GetHandle);
+    stub_proc_address(DetachObject);
+    stub_proc_address(CreateShaderObject);
+    stub_proc_address(ShaderSource);
+    stub_proc_address(CompileShader);
+    stub_proc_address(CreateProgramObject);
+    stub_proc_address(AttachObject);
+    stub_proc_address(LinkProgram);
+    stub_proc_address(UseProgramObject);
+    stub_proc_address(ValidateProgram);
+    stub_proc_address(Uniform1f);
+    stub_proc_address(Uniform2f);
+    stub_proc_address(Uniform3f);
+    stub_proc_address(Uniform4f);
+    stub_proc_address(Uniform1i);
+    stub_proc_address(Uniform2i);
+    stub_proc_address(Uniform3i);
+    stub_proc_address(Uniform4i);
+    stub_proc_address(Uniform1fv);
+    stub_proc_address(Uniform2fv);
+    stub_proc_address(Uniform3fv);
+    stub_proc_address(Uniform4fv);
+    stub_proc_address(Uniform1iv);
+    stub_proc_address(Uniform2iv);
+    stub_proc_address(Uniform3iv);
+    stub_proc_address(Uniform4iv);
+    stub_proc_address(UniformMatrix2fv);
+    stub_proc_address(UniformMatrix3fv);
+    stub_proc_address(UniformMatrix4fv);
+    stub_proc_address(GetObjectParameterfv);
+    stub_proc_address(GetObjectParameteriv);
+    stub_proc_address(GetInfoLog);
+    stub_proc_address(GetAttachedObjects);
+    stub_proc_address(GetUniformLocation);
+    stub_proc_address(GetActiveUniform);
+    stub_proc_address(GetUniformfv);
+    stub_proc_address(GetUniformiv);
+    stub_proc_address(GetShaderSource);
+    stub_proc_address(BindBuffer);
+    stub_proc_address(DeleteBuffers);
+    stub_proc_address(GenBuffers);
+    stub_proc_address(IsBuffer);
+    stub_proc_address(BufferData);
+    stub_proc_address(BufferSubData);
+    stub_proc_address(GetBufferSubData);
+    stub_proc_address(MapBuffer);
+    stub_proc_address(UnmapBuffer);
+    stub_proc_address(GetBufferParameteriv);
+    stub_proc_address(GetBufferPointerv);
+  }
+  if(!have_ARB_vertex_shader) {
+    stub_proc_address(VertexAttrib1f);
+    stub_proc_address(VertexAttrib1s);
+    stub_proc_address(VertexAttrib1d);
+    stub_proc_address(VertexAttrib2f);
+    stub_proc_address(VertexAttrib2s);
+    stub_proc_address(VertexAttrib2d);
+    stub_proc_address(VertexAttrib3f);
+    stub_proc_address(VertexAttrib3s);
+    stub_proc_address(VertexAttrib3d);
+    stub_proc_address(VertexAttrib4f);
+    stub_proc_address(VertexAttrib4s);
+    stub_proc_address(VertexAttrib4d);                               
+    stub_proc_address(VertexAttrib4Nub);
+    stub_proc_address(VertexAttrib1fv);
+    stub_proc_address(VertexAttrib1sv);
+    stub_proc_address(VertexAttrib1dv);
+    stub_proc_address(VertexAttrib2fv);
+    stub_proc_address(VertexAttrib2sv);
+    stub_proc_address(VertexAttrib2dv);
+    stub_proc_address(VertexAttrib3fv);
+    stub_proc_address(VertexAttrib3sv);
+    stub_proc_address(VertexAttrib3dv);
+    stub_proc_address(VertexAttrib4fv);
+    stub_proc_address(VertexAttrib4sv);
+    stub_proc_address(VertexAttrib4dv);
+    stub_proc_address(VertexAttrib4iv);
+    stub_proc_address(VertexAttrib4bv);
+    stub_proc_address(VertexAttrib4ubv);
+    stub_proc_address(VertexAttrib4usv);
+    stub_proc_address(VertexAttrib4uiv);
+    stub_proc_address(VertexAttrib4Nbv);
+    stub_proc_address(VertexAttrib4Nsv);
+    stub_proc_address(VertexAttrib4Niv);
+    stub_proc_address(VertexAttrib4Nubv);
+    stub_proc_address(VertexAttrib4Nusv);
+    stub_proc_address(VertexAttrib4Nuiv);
+    stub_proc_address(VertexAttribPointer);
+    stub_proc_address(EnableVertexAttribArray);
+    stub_proc_address(DisableVertexAttribArray);
+    stub_proc_address(BindAttribLocation);
+    stub_proc_address(GetActiveAttrib);
+    stub_proc_address(GetAttribLocation);
+    stub_proc_address(GetVertexAttribdv);
+    stub_proc_address(GetVertexAttribfv);
+    stub_proc_address(GetVertexAttribiv);
+    stub_proc_address(GetVertexAttribPointerv);
   }
 }
--- xgl.hh
+++ xgl.hh
@@ -29,7 +29,136 @@ namespace xgl {
   EXTERN void(*GetBufferPointerv)(GLenum target, GLenum pname, void** v);
   /* ARB_texture_rectangle */
   EXTERN bool have_ARB_texture_rectangle;
+  /* ARB_shader_objects */
+  EXTERN bool have_ARB_shader_objects;
+  EXTERN void DeleteObject(GLhandleARB obj);
+  EXTERN GLhandleARB GetHandle(GLenum pname);
+  EXTERN void DetachObject(GLhandleARB containerObj, GLhandleARB attachedObj);
+  EXTERN GLhandleARB CreateShaderObject(GLenum shaderType);
+  EXTERN void ShaderSource(GLhandleARB shaderObj, GLsizei count,
+                    const GLcharARB **string, const GLint *length);
+  EXTERN void CompileShader(GLhandleARB shaderObj);
+  EXTERN GLhandleARB CreateProgramObject(void);
+  EXTERN void AttachObject(GLhandleARB containerObj, GLhandleARB obj);
+  EXTERN void LinkProgram(GLhandleARB programObj);
+  EXTERN void UseProgramObject(GLhandleARB programObj);
+  EXTERN void ValidateProgram(GLhandleARB programObj);
+  EXTERN void Uniform1f(GLint location, GLfloat v0);
+  EXTERN void Uniform2f(GLint location, GLfloat v0, GLfloat v1);
+  EXTERN void Uniform3f(GLint location, GLfloat v0, GLfloat v1, GLfloat v2);
+  EXTERN void Uniform4f(GLint location,
+                        GLfloat v0, GLfloat v1,
+                        GLfloat v2, GLfloat v3);
+  EXTERN void Uniform1i(GLint location, GLint v0);
+  EXTERN void Uniform2i(GLint location, GLint v0, GLint v1);
+  EXTERN void Uniform3i(GLint location, GLint v0, GLint v1, GLint v2);
+  EXTERN void Uniform4i(GLint location,
+                        GLint v0, GLint v1, GLint v2, GLint v3);
+  EXTERN void Uniform1fv(GLint location, GLsizei count, const GLfloat *value);
+  EXTERN void Uniform2fv(GLint location, GLsizei count, const GLfloat *value);
+  EXTERN void Uniform3fv(GLint location, GLsizei count, const GLfloat *value);
+  EXTERN void Uniform4fv(GLint location, GLsizei count, const GLfloat *value);
+  EXTERN void Uniform1iv(GLint location, GLsizei count, const GLint *value);
+  EXTERN void Uniform2iv(GLint location, GLsizei count, const GLint *value);
+  EXTERN void Uniform3iv(GLint location, GLsizei count, const GLint *value);
+  EXTERN void Uniform4iv(GLint location, GLsizei count, const GLint *value);
+  EXTERN void UniformMatrix2fv(GLint location, GLsizei count,
+                               GLboolean transpose, const GLfloat *value);
+  EXTERN void UniformMatrix3fv(GLint location, GLsizei count,
+                               GLboolean transpose, const GLfloat *value);
+  EXTERN void UniformMatrix4fv(GLint location, GLsizei count,
+                               GLboolean transpose, const GLfloat *value);
+  EXTERN void GetObjectParameterfv(GLhandleARB obj, GLenum pname,
+                                   GLfloat *params);
+  EXTERN void GetObjectParameteriv(GLhandleARB obj, GLenum pname,
+                                   GLint *params);
+  EXTERN void GetInfoLog(GLhandleARB obj, GLsizei maxLength,
+                         GLsizei *length, GLcharARB *infoLog);
+  EXTERN void GetAttachedObjects(GLhandleARB containerObj, GLsizei maxCount,
+                                 GLsizei *count, GLhandleARB *obj);
+  EXTERN GLint GetUniformLocation(GLhandleARB programObj,
+                                  const GLcharARB *name);
+  EXTERN void GetActiveUniform(GLhandleARB programObj, uGLint index,
+                               GLsizei maxLength, GLsizei *length, GLint *size,
+                               ELenum *type, GLcharARB *name);
+  EXTERN void GetUniformfv(GLhandleARB programObj, GLint location,
+                           GLfloat *params);
+  EXTERN void GetUniformiv(GLhandleARB programObj, GLint location,
+                           GLint *params);
+  EXTERN void GetShaderSource(GLhandleARB obj, GLsizei maxLength,
+                              GLsizei *length, GLcharARB *source);
+  /* ARB_fragment_shader */
+  EXTERN bool have_ARB_fragment_shader;
+  /* ARB_vertex_shader */
+  EXTERN bool have_ARB_vertex_shader;
+  EXTERN void VertexAttrib1f(GLuint index, GLfloat v0);
+  EXTERN void VertexAttrib1s(GLuint index, GLshort v0);
+  EXTERN void VertexAttrib1d(GLuint index, GLdouble v0);
+  EXTERN void VertexAttrib2f(GLuint index, GLfloat v0, GLfloat v1);
+  EXTERN void VertexAttrib2s(GLuint index, GLshort v0, GLshort v1);
+  EXTERN void VertexAttrib2d(GLuint index, GLdouble v0, GLdouble v1);
+  EXTERN void VertexAttrib3f(GLuint index,
+                             GLfloat v0, GLfloat v1, GLfloat v2);
+  EXTERN void VertexAttrib3s(GLuint index,
+                             GLshort v0, GLshort v1, GLshort v2);
+  EXTERN void VertexAttrib3d(GLuint index,
+                             GLdouble v0, GLdouble v1, GLdouble v2);
+  EXTERN void VertexAttrib4f(GLuint index,
+                             GLfloat v0, GLfloat v1,
+                             GLfloat v2, GLfloat v3);
+  EXTERN void VertexAttrib4s(GLuint index,
+                             GLshort v0, GLshort v1,
+                             GLshort v2, GLshort v3);
+  EXTERN void VertexAttrib4d(GLuint index,
+                             GLdouble v0, GLdouble v1,
+                             GLdouble v2, GLdouble v3);
+  EXTERN void VertexAttrib4Nub(GLuint index,
+                               GLubyte x, GLubyte y, GLubyte z, GLubyte w);
+  EXTERN void VertexAttrib1fv(GLuint index, const GLfloat *v);
+  EXTERN void VertexAttrib1sv(GLuint index, const GLshort *v);
+  EXTERN void VertexAttrib1dv(GLuint index, const GLdouble *v);
+  EXTERN void VertexAttrib2fv(GLuint index, const GLfloat *v);
+  EXTERN void VertexAttrib2sv(GLuint index, const GLshort *v);
+  EXTERN void VertexAttrib2dv(GLuint index, const GLdouble *v);
+  EXTERN void VertexAttrib3fv(GLuint index, const GLfloat *v);
+  EXTERN void VertexAttrib3sv(GLuint index, const GLshort *v);
+  EXTERN void VertexAttrib3dv(GLuint index, const GLdouble *v);
+  EXTERN void VertexAttrib4fv(GLuint index, const GLfloat *v);
+  EXTERN void VertexAttrib4sv(GLuint index, const GLshort *v);
+  EXTERN void VertexAttrib4dv(GLuint index, const GLdouble *v);
+  EXTERN void VertexAttrib4iv(GLuint index, const GLint *v);
+  EXTERN void VertexAttrib4bv(GLuint index, const GLbyte *v);
+  EXTERN void VertexAttrib4ubv(GLuint index, const GLubyte *v);
+  EXTERN void VertexAttrib4usv(GLuint index, const GLushort *v);
+  EXTERN void VertexAttrib4uiv(GLuint index, const GLuint *v);
+  EXTERN void VertexAttrib4Nbv(GLuint index, const GLbyte *v);
+  EXTERN void VertexAttrib4Nsv(GLuint index, const GLshort *v);
+  EXTERN void VertexAttrib4Niv(GLuint index, const GLint *v);
+  EXTERN void VertexAttrib4Nubv(GLuint index, const GLubyte *v);
+  EXTERN void VertexAttrib4Nusv(GLuint index, const GLushort *v);
+  EXTERN void VertexAttrib4Nuiv(GLuint index, const GLuint *v);
+  EXTERN void VertexAttribPointer(GLuint index, GLint size, GLenum type,
+                                  boolean normalized, GLsizei stride,
+                                  const EXTERN void *pointer);
+  EXTERN void EnableVertexAttribArray(GLuint index);
+  EXTERN void DisableVertexAttribArray(GLuint index);
+  EXTERN void BindAttribLocation(GLhandleARB programObj,
+                                 GLuint index, const charARB *name);
+  EXTERN void GetActiveAttrib(GLhandleARB programObj, GLuint index,
+                              GLsizei maxLength, GLsizei *length,
+                              GLint *size, GLenum *type, GLcharARB *name);
+  EXTERN GLint GetAttribLocation(GLhandleARB programObj,
+                                 const GLcharARB *name);
+  EXTERN void GetVertexAttribdv(GLuint index,
+                                GLenum pname, GLdouble *params);
+  EXTERN void GetVertexAttribfv(GLuint index,
+                                GLenum pname, GLfloat *params);
+  EXTERN void GetVertexAttribiv(GLuint index, GLenum pname, GLint *params);
+  EXTERN void GetVertexAttribPointerv(GLuint index,
+                                      GLenum pname, void **pointer);
 };
+
+#define REQUIRE_EXTENSION(ext if(!xgl::have_##ext) die("Your video hardware does not support %s, and is therefore not supported.", #ext)
 
 #undef EXTERN
 
