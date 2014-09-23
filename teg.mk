576e832e4f44d28a23056257f2f3ddf8666d0eaf
Solra Bizna <solra@bizna.name>
2014-09-22 21:43:48 -0600

    Some meaningless changes that may become meaningful later

--- teg.mk
+++ teg.mk
@@ -1,7 +1,9 @@
 AR=ar
 ARFLAGS=-rscD
 
-lib/libteg.a: obj/teg/config.o obj/teg/io.o obj/teg/miscutil.o obj/teg/video.o obj/teg/xgl.o obj/teg/main.o
+TEG_OBJECTS:=obj/teg/config.o obj/teg/io.o obj/teg/miscutil.o obj/teg/video.o obj/teg/xgl.o obj/teg/main.o
+
+lib/libteg.a: $(TEG_OBJECTS)
 	@echo Archiving "$@"...
 	@$(AR) $(ARFLAGS) "$@" $^
 	@test -f obj/teg/config.debug.o && $(AR) $(ARFLAGS) $(patsubst %.a,%.debug.a,$@) $(patsubst %.o,%.debug.o,$^)
